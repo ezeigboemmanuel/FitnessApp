3803cb05dbac7681e852f7223724d0d36bda663e
ezeigboemmanuel <ezeigboemmanuel2@gmail.com>
2023-09-17 11:11:57 +0100

    finished the navbar component and the banner

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import bannerImage from "../assets/image/fitnessimg.jpg"
+import Image from 'next/image'
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className="banner">
+        <Image src={bannerImage} alt="banner-img" fill className="banner-img" />
+        <div>
+            <p  className="banner-text">GET A HEALTHY BODY WITH THE PERFECT EXERCISES</p>
+        </div>
+        <div className="banner-search">
+            <input type="text" className="banner-input" placeholder='Search Exercises...' />
+            <button className="banner-btn">Search</button>
+        </div>
+    </div>
   )
 }
 
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import  '../styles.css'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar-wrapper">
+        <div className="logo"><Link href="/" className="nav-link">FitLord</Link></div>
+        <div className="nav-links">
+            <Link href="/" className="nav-link">Home</Link>
+            <Link href="/categories" className="nav-link">Categories</Link>
+            <Link href="/about" className="nav-link">About</Link>
+        </div>
+    </div>
   )
 }
 
